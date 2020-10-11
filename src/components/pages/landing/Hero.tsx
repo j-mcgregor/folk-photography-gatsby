f6d043e86e372cd8937cfe1d87dd2a8d7e960cbb
Jack <jmcgregor@coveo.com>
2020-10-11 14:49:51 -0400

    remove styled hero

--- src/components/pages/landing/Hero.tsx
+++ src/components/pages/landing/Hero.tsx
@@ -1,48 +1,18 @@
 import * as React from 'react'
-import styled from 'styled-components'
 
-import { Breakpoints, HeroProps, StyledHeroProps } from '../../../types/enums'
-
-const StyledHero = styled.div<StyledHeroProps>`
-    background-size: cover;
-    background-position: right center;
-    background-repeat: no-repeat;
-    background-attachment: fixed;
-    height: 100vh;
-    text-transform: uppercase;
-    animation: fadeIn 0.5s ease-in;
-
-    h3 {
-        font-family: 'Abel', sans-serif;
-    }
-
-    @media screen and (max-width: ${Breakpoints.md}px) {
-        height: 600px;
-    }
-    @media screen and (max-width: ${Breakpoints.sm}px) {
-        height: 500px;
-    }
-
-    @keyframes fadeIn {
-        from {
-            opacity: 0;
-        }
-        to {
-            opacity: 1;
-        }
-    }
-`
+import { HeroProps } from '../../../types/enums'
 
 const Hero: React.FC<HeroProps> = ({ logo, backgroundImage }) => {
     return (
         backgroundImage && (
-            <StyledHero
-                bgImageUrl={backgroundImage}
-                className="flex flex-column flex-center"
-                style={{ backgroundImage: `url(${backgroundImage})` }}
+            <div
+                className="flex flex-column flex-center hero"
+                style={{
+                    backgroundImage: `url(${backgroundImage})`,
+                }}
             >
                 {logo}
-            </StyledHero>
+            </div>
         )
     )
 }
